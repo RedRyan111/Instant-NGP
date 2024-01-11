e975e35c521a2cf3a9c13830b5cb9afef7976b4d
RedRyan111 <ravenengineerryan@gmail.com>
2024-01-10 22:24:16 -0600

    Refactor corner embeddings

--- Tests/test_voxel_hash.py
+++ Tests/test_voxel_hash.py
@@ -22,17 +22,30 @@ xyz_tensor = torch.cat([torch.zeros(1, 3)-1.49, torch.zeros(1, 3)+1.49], dim=0)
 print(f'xyz tensor: ')
 print(xyz_tensor.shape)
 
-corner_embeddings = voxel_hash.get_corner_embedding_vectors(xyz_tensor)
+#corner_embeddings = voxel_hash.get_corner_embedding_vectors(xyz_tensor)
 
 print(f'starting!')
 
-print(f'embedding: {corner_embeddings}')
+#print(f'embedding: {corner_embeddings}')
 
 
 xyz_embeddings = voxel_hash.get_embedding(xyz_tensor)
 
-rand_tensor = torch.rand((2, 8, 2))
-temp_1 = torch.rand((2, 2)).unsqueeze(1).expand_as(rand_tensor)
-print(f'temp 1 shape: {temp_1.shape}')
+#rand_tensor = torch.rand((2, 8, 2))
+#temp_1 = torch.rand((2, 2)).unsqueeze(1).expand_as(rand_tensor)
+#print(f'temp 1 shape: {temp_1.shape}')
 
 #print(rand_tensor - temp_1)
+
+'''
+cube_of_indecis = torch.stack([
+    torch.stack([torch.ones(2), torch.ones(2)*2, torch.ones(2)*3], dim=1),
+    torch.stack([torch.ones(2), torch.ones(2)*2, torch.ones(2)*3], dim=1),
+    torch.stack([torch.ones(2), torch.ones(2)*2, torch.ones(2)*3], dim=1)
+], dim=1)
+
+print(cube_of_indecis)
+print(f'cube shape: {cube_of_indecis.shape}')
+print(f'only x:')
+print(cube_of_indecis[:, :, 0])
+'''
