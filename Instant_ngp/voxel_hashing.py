507ed59ef007aa6df016c375d3b73b5094c547ef
RedRyan111 <ravenengineerryan@gmail.com>
2024-01-10 22:32:06 -0600

    Delete unused code

--- Instant_ngp/voxel_hashing.py
+++ Instant_ngp/voxel_hashing.py
@@ -40,12 +40,12 @@ class VoxelHash(nn.Module):
         cube_of_xyz_coords = self.get_cube_of_xyz_coords(xyz_tensor)
         print(f'cube of coords: {cube_of_xyz_coords.shape}')
 
-        corner_embeddings = self.get_corner_embeddings_from_cube_of_indecis(cube_of_xyz_coords)
+        corner_embeddings = self.get_corner_embeddings_from_cube_of_xyz_coords(cube_of_xyz_coords)
         print(f'corner_embeddings: {corner_embeddings.shape}')
 
+        #trilinearyly interpolate corner embeddings and xyz_tensor
 
         corrected_xyz_tensor = corrected_xyz_tensor.unsqueeze(1).expand_as(cube_of_xyz_coords)
-
         print(f'corrected xyz: {corrected_xyz_tensor.shape}')
 
         sub_vectors = (cube_of_xyz_coords - corrected_xyz_tensor)**2
@@ -58,10 +58,8 @@ class VoxelHash(nn.Module):
         normalized_distance = distance_vectors / sum_of_distances
         print(f'normalized distance vectors: {normalized_distance.shape}')
 
-        final_embeddings = torch.sum(normalized_distance.unsqueeze(2) * corner_embeddings, dim=1)
+        final_embeddings = torch.sum(normalized_distance.unsqueeze(2) * corner_embeddings, dim=1) #weighted sum
         print(f'final embedding: {final_embeddings.shape}')
-
-        # trilinearly interpolate
 
         return final_embeddings
 
@@ -97,61 +95,17 @@ class VoxelHash(nn.Module):
         multiplier = self.resolution / self.size
         return multiplier * xyz_tensor + self.resolution / 2
 
-    def get_corner_embeddings_from_cube_of_indecis(self, cube_of_indecis):
+    def get_corner_embeddings_from_cube_of_xyz_coords(self, cube_of_xyz_coords):
 
-        print(f'cube of indecis for embeddings: {cube_of_indecis.shape}')
+        print(f'cube of indecis for embeddings: {cube_of_xyz_coords.shape}')
 
-        cube_of_indecis[:, :, 1] = cube_of_indecis[:, :, 1] * self.size
-        cube_of_indecis[:, :, 2] = cube_of_indecis[:, :, 2] * self.size**2
+        cube_of_xyz_coords[:, :, 1] = cube_of_xyz_coords[:, :, 1] * self.size
+        cube_of_xyz_coords[:, :, 2] = cube_of_xyz_coords[:, :, 2] * self.size ** 2
 
-        indecis = torch.sum(cube_of_indecis, dim=2, dtype=torch.int)
+        indecis = torch.sum(cube_of_xyz_coords, dim=2, dtype=torch.int)
 
         print(f'indecis: {indecis.shape}')
         print(indecis)
         embeddings = self.embedding(indecis)
 
         return embeddings
-
-'''
-    def get_corner_embedding_vectors(self, xyz_tensor):
-        multiplier = self.resolution / self.size
-        xyz_tensor = multiplier * xyz_tensor + self.resolution/2
-
-        print(f'xyz tensor shape: {xyz_tensor.shape}')
-
-        xyz_floor = torch.floor(xyz_tensor)
-        xyz_ceil = torch.ceil(xyz_tensor)
-
-        bot_x_index = xyz_floor[:, 0]
-        top_x_index = xyz_ceil[:, 0]
-        bot_y_index = xyz_floor[:, 1] * self.size
-        top_y_index = xyz_ceil[:, 1] * self.size
-        bot_z_index = xyz_floor[:, 2] * self.size**2
-        top_z_index = xyz_ceil[:, 2] * self.size**2
-
-        print(f'bot x: {bot_x_index.shape}')
-
-        #need to use this cube to get the weights for trilinear interpolation
-        cube_of_indecis = torch.stack([
-            torch.stack([bot_x_index, bot_y_index, bot_z_index], dim=1),
-            torch.stack([bot_x_index, bot_y_index, top_z_index], dim=1),
-            torch.stack([bot_x_index, top_y_index, bot_z_index], dim=1),
-            torch.stack([bot_x_index, top_y_index, top_z_index], dim=1),
-            torch.stack([top_x_index, bot_y_index, bot_z_index], dim=1),
-            torch.stack([top_x_index, bot_y_index, top_z_index], dim=1),
-            torch.stack([top_x_index, top_y_index, bot_z_index], dim=1),
-            torch.stack([top_x_index, top_y_index, top_z_index], dim=1)
-        ], dim=1)
-
-        print(f'cube of indecis: {cube_of_indecis.shape}')
-
-        indecis = torch.sum(cube_of_indecis, dim=2, dtype=torch.int)
-
-        print(f'indecis: {indecis.shape}')
-
-        embeddings = self.embedding(indecis)
-        print(f'embeddings: {embeddings.shape}')
-        #embedding = torch.cat(embeddings, dim=1)
-
-        return embeddings
-'''
